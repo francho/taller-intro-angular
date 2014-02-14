8124087771974e41369995bc0b588bb659f5a888
Francho <lab@francho.org>
2014-02-14 20:38:02 +0100

    directiva zgzRestaurante

---
+++ app/scripts/zgzRestaurantesDirective.js
@@ -0,0 +1,15 @@
+'use strict';
+
+zgzApp.directive('zgzRestaurante', function () {
+    return {
+      templateUrl: 'views/partials/restaurante.html',
+      restrict: 'E',
+      scope: {
+        'restaurante': '=ngModel',
+        'verTodo': '&'
+      },
+      controller : function($scope) {
+        $scope.verTodo = false;
+      }
+    };
+  });
