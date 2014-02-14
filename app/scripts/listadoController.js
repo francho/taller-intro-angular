fa511cfcfed685f5608933731d397165d9e2a8cb
Francho <lab@francho.org>
2014-02-14 20:27:22 +0100

    mas filtros

--- app/scripts/listadoController.js
+++ app/scripts/listadoController.js
@@ -4,10 +4,17 @@
 zgzApp.controller('ListadoController', function ($scope, RestaurantesService) {
   $scope.restaurantes = RestaurantesService.getRestaurantes();
 
-  $scope.limite = 10;
+  $scope.orden = 'title';
+  $scope.ordenInverso = false;
+  $scope.busqueda = '';
+  $scope.limite = 5;
 
-  $scope.verMas = function() {
-    $scope.limite += 10;
+  $scope.verMas = function () {
+    $scope.limite += 5;
+  }
+
+  $scope.verMenos = function () {
+    $scope.limite -= ($scope.limite > 5) ? 5 : 0;
   }
 
 });
