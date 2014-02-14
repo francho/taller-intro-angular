e8629b4854c5d5da3f688fe497de1f1a5cbcee85
Francho <lab@francho.org>
2014-02-14 20:21:07 +0100

    boton de verMas

--- app/scripts/listadoController.js
+++ app/scripts/listadoController.js
@@ -4,4 +4,10 @@
 zgzApp.controller('ListadoController', function ($scope, RestaurantesService) {
   $scope.restaurantes = RestaurantesService.getRestaurantes();
 
+  $scope.limite = 10;
+
+  $scope.verMas = function() {
+    $scope.limite += 10;
+  }
+
 });
